31617a608e78f0277d81ff54647f29a8b990e689
Austin Sharman <asharman22@gmail.com>
2022-06-12 18:57:20 -0400

    add random movement for enemies

---
+++ src/systems/random_move.rs
@@ -0,0 +1,21 @@
+use crate::prelude::*;
+
+#[system]
+#[write_component(Point)]
+#[read_component(MovingRandomly)]
+pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
+    let mut movers = <(&mut Point, &MovingRandomly)>::query();
+    movers.for_each_mut(ecs, |(pos, _)| {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range(0, 4) {
+            0 => Point::new(-1, 0),
+            1 => Point::new(1, 0),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
+        } + *pos;
+
+        if map.can_enter_tile(destination) {
+            *pos = destination;
+        }
+    })
+}
